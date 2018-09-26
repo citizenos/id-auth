b881839542f92d3944c20baf8b6a61d426caf8be
Mikk Andresen <mikk.andresen@gmail.com>
2018-09-26 13:39:53 +0300

    WIP: Check key usage, tests need a new certificate to pass.

--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,16 @@ module.exports = function (app) {
                     status: checkCertificateResult.Status.$value
                 };
 
+                // Check the certificate usage, see if it's meant for client authentication at all
+                // https://www.ibm.com/support/knowledgecenter/en/SSKTMJ_9.0.1/admin/conf_keyusageextensionsandextendedkeyusage_r.html
+                if (!checkCertificateResult.EnhancedKeyUsage || !checkCertificateResult.EnhancedKeyUsage.$value || checkCertificateResult.EnhancedKeyUsage.$value.indexOf('TLS Web Client Authentication') < 0) {
+                    logger.error('Unexpected certificate enhanced key usage', checkCertificateResult.EnhancedKeyUsage, 'expecting TLS Web Client Authentication');
+
+                    res.badRequest('Invalid certificate enhanced key usage. Expecting "TLS Web Client Authentication"');
+
+                    return Promise.reject();
+                }
+
                 switch (data.status) { // GOOD, UNKNOWN, EXPIRED, SUSPENDED
                     case 'GOOD':
                         data.user = {
